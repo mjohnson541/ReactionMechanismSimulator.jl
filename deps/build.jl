0cd97dd423224f5318f12627f71bf197525d6de2
Matt Johnson <mjohnson541@gmail.com>
2020-07-30 14:03:41 -0400

    tweak

--- deps/build.jl
+++ deps/build.jl
@@ -3,6 +3,7 @@ if PyCall.pyversion.major != 3 || PyCall.pyversion.minor != 7
     using Conda
     const Pkg = Base.require(Base.PkgId(Base.UUID("44cfe95a-1eb2-52ea-b672-e2afdf69b78f"), "Pkg"))
     Conda.add_channel("rmg")
+    Conda.add_channel("defaults")
     Conda.add("python==3.7")
     Conda.clean()
     Conda.update()
